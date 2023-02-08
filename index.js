93eae9f73e4aacc494ccbd8922945284f034fcba
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-08 16:39:00 -0600

    cors order switch

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const routes = require('./routes/UserRoute')
 const app = express();
 require('dotenv').config()
 app.use(express.json())
-app.use(routes)
 app.use(cors())
+app.use(routes)
 const PORT = process.env.PORT || 8080;
 
 
